0322384afb56b0fef8616c514f2421d683671f1b
pc_lab_s3m <gabriel.novas@aimen.es>
2026-02-11 14:03:35 +0100

    Opensim coord system fix

--- camera_to_opensim/launch/convert.launch.py
+++ camera_to_opensim/launch/convert.launch.py
@@ -17,19 +17,19 @@ def generate_launch_description():
     
     output_dir_arg = DeclareLaunchArgument(
         'output_dir',
-        default_value=os.path.join(os.path.expanduser('~'), 'opensim_data'),
+        default_value='~/drims_ws/simulation_projects/opensim_results',
         description='Directory to save TRC and OpenSim Data'
     )
     
     pose2sim_path_arg = DeclareLaunchArgument(
         'pose2sim_path',
-        default_value='/home/drims/static/drims2_ws/src/camera_to_opensim_core/pose2sim',
+        default_value='~/static/drims2_ws/src/camera_to_opensim_core/pose2sim',
         description='Path to Pose2Sim repository (if not installed)'
     )
     
     sports2d_path_arg = DeclareLaunchArgument(
         'sports2d_path',
-        default_value='/home/drims/static/drims2_ws/src/camera_to_opensim_core/Sports2D',
+        default_value='~/static/drims2_ws/src/camera_to_opensim_core/Sports2D',
         description='Path to Sports2D repository (if not installed)'
     )
 
